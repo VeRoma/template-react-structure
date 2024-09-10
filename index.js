ef0eb7d4e0a4c53c58a55254b868bc9f30d550a0
VeRoma <r.vetokhin@gmail.com>
2024-09-10 23:00:21 +0600

    correct code

--- index.js
+++ index.js
@@ -18,14 +18,10 @@ const targetPath = path.resolve(process.cwd(), appName);
 const templatePath = path.join(__dirname, 'template');
 const packageJsonPath = path.join(templatePath, 'package.json');
 
-// Проверяем, установлен ли degit
-function installDegit() {
-  try {
-    execSync('npx degit --help', { stdio: 'inherit' });
-  } catch (e) {
-    console.log('Устанавливаем degit...');
-    execSync('npm install degit --global', { stdio: 'inherit' });
-  }
+// Проверка на существование директории
+if (fs.existsSync(targetPath)) {
+  console.error(`Ошибка: Директория ${targetPath} уже существует`);
+  process.exit(1);
 }
 
 // Обновляем поле "name" в package.json
@@ -33,10 +29,13 @@ async function updatePackageJson(appName) {
   try {
     const packageJson = await fs.readJson(packageJsonPath);
     packageJson.name = appName; // Устанавливаем имя проекта
-    await fs.writeJson(packageJsonPath, packageJson, { spaces: 2 });
+    const targetPackageJsonPath = path.join(targetPath, 'package.json');
+    
+    await fs.writeJson(targetPackageJsonPath, packageJson, { spaces: 2 });
     console.log(`Файл package.json обновлен с именем проекта: ${appName}`);
   } catch (error) {
     console.error('Ошибка при обновлении package.json:', error);
+    process.exit(1);
   }
 }
 
@@ -45,11 +44,11 @@ async function createProject() {
   try {
     console.log(`Создание проекта в директории ${targetPath}...`);
 
-    // Обновляем package.json перед копированием
-    await updatePackageJson(appName);
-
     // Копируем файлы в целевую директорию
     await fs.copy(templatePath, targetPath);
+
+    // Обновляем package.json после копирования
+    await updatePackageJson(appName);
 
     console.log('Файлы успешно скопированы.');
 
@@ -59,7 +58,7 @@ async function createProject() {
     console.log('Устанавливаем зависимости...');
     execSync('npm install', { stdio: 'inherit' });
 
-	console.log('Обновляем зависимости...');
+    console.log('Обновляем зависимости...');
     execSync('npx npm-check-updates --target minor -u', { stdio: 'inherit' });
 
     console.log('Проект успешно создан с обновлёнными зависимостями');
@@ -69,11 +68,11 @@ async function createProject() {
     execSync('code . --reuse-window', { stdio: 'inherit' });
   } catch (error) {
     console.error('Ошибка:', error);
+    process.exit(1);
   }
 }
 
 // Основная логика выполнения
 (async () => {
-  installDegit();
   await createProject();
 })();
