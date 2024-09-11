4aba9a77ec9c83b87f5816065a132327710ddbcf
VeRoma <r.vetokhin@gmail.com>
2024-09-11 18:59:35 +0600

    add code text in mount files

--- template/.vscode/create_and_open.js
+++ template/.vscode/create_and_open.js
@@ -1,26 +1,55 @@
-const fs = require('fs');
-const { exec } = require('child_process');
-const path = require('path');
+const fs = require("fs");
+const { exec } = require("child_process");
+const path = require("path");
 
 // Функция для капитализации первой буквы
-const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+const capitalizeFirstLetter = (string) =>
+	string.charAt(0).toUpperCase() + string.slice(1);
 
 const folderName = process.argv[2];
-const createCssFile = process.argv.length > 3 && process.argv[3] === 'css';
+const createCssFile = process.argv.length > 3 && process.argv[3] === "css";
 const capitalizedName = capitalizeFirstLetter(folderName);
 
-const componentPath = path.join('src', 'components', capitalizedName);
+const componentPath = path.join("src", "components", capitalizedName);
 
 // Создаем директорию и файлы
 fs.mkdirSync(componentPath, { recursive: true });
-fs.writeFileSync(path.join(componentPath, `${capitalizedName}.jsx`), '');
+
+// Шаблон JSX-кода компонента
+let componentCode = `
+import React from 'react';
+${createCssFile ? `import './${capitalizedName}.module.scss';\n` : ""}
+const ${capitalizedName} = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ${capitalizedName};
+`;
+
+// Записываем JSX-файл с содержимым
+fs.writeFileSync(
+	path.join(componentPath, `${capitalizedName}.jsx`),
+	componentCode.trim()
+);
+
+// Если указан флаг 'css', создаем SCSS-файл
 if (createCssFile) {
-    fs.writeFileSync(path.join(componentPath, `${capitalizedName}.module.scss`), '');
+	fs.writeFileSync(
+		path.join(componentPath, `${capitalizedName}.module.scss`),
+		""
+	);
 }
 
 // Открываем файл в VSCode
-exec(`code -r "${path.join(componentPath, `${capitalizedName}.jsx`)}"`, (err) => {
-    if (err) {
-        console.error(err);
-    }
-});
+exec(
+	`code -r "${path.join(componentPath, `${capitalizedName}.jsx`)}"`,
+	(err) => {
+		if (err) {
+			console.error(err);
+		}
+	}
+);
